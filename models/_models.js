5e2bbcec1c1f1d1c00ee7a1a16399674cd5466bb
ownfrezzy <ownfrezzy@gmail.com>
2021-09-07 16:34:27 +0300

    +relations

--- models/_models.js
+++ models/_models.js
@@ -9,3 +9,41 @@ const Users = require("./users");
 const UsersModules = require("./usersmodules");
 const UserTasks = require("./usertasks");
 
+Users.hasMany(SocialNetworks, { foreignKey: "user_id" });
+SocialNetworks.belongsTo(Users, { foreignKey: "user_id" });
+
+Statuses.hasMany(UserTasks, { foreignKey: "status_id" });
+UserTasks.belongsTo(Statuses, { foreignKey: "status_id" });
+
+Users.belongsToMany(Tasks, { through: "UserTasks" });
+Tasks.belongsToMany(Users, { through: "UserTasks" });
+
+Users.belongsToMany(Modules, { through: "UsersModules" });
+Modules.belongsToMany(Users, { through: "UsersModules" });
+
+Modules.hasMany(Tasks, { foreignKey: "module_id" });
+Tasks.belongsTo(Modules, { foreignKey: "module_id" });
+
+Modules.hasMany(Checklists, { foreignKey: "module_id" });
+Checklists.belongsTo(Modules, { foreignKey: "module_id" });
+
+Modules.hasMany(Themes, { foreignKey: "module_id" });
+Themes.belongsTo(Modules, { foreignKey: "module_id" });
+
+Checklists.belongsToMany(Themes, { through: "ThemesChecklist" });
+Themes.belongsToMany(Checklists, { through: "ThemesChecklist" });
+
+const _modules = {
+    Checklists,
+    Modules,
+    SocialNetworks,
+    Statuses,
+    Tasks,
+    Themes,
+    ThemesChecklist,
+    Users,
+    UsersModules,
+    UserTasks
+}
+
+module.exports = _modules
